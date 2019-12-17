4b4cf9891ad126268cbf64e50300777f08ddcf21
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-17 19:01:26 +0100

    Commit 36

--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -1,12 +1,16 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 
+// Modules
+import { SharedModule } from '../shared/shared.module';
 
+// Components
+import { EventListComponent } from './event-list/event-list.component';
+import { EventDetailsComponent } from './event-details/event-details.component';
 
 @NgModule({
-  declarations: [],
-  imports: [
-    CommonModule
-  ]
+  imports: [CommonModule, SharedModule],
+  declarations: [EventListComponent, EventDetailsComponent],
+  exports: [EventListComponent, EventDetailsComponent]
 })
-export class EventsModule { }
+export class EventsModule {}
