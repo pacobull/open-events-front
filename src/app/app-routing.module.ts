c06d12733f93642ab343b3ba9de5e833f71fc94d
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-17 21:07:33 +0100

    Commit 37

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,21 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+
+import { LandingPageComponent } from './landing-page/landing-page.component';
+import { EventListComponent } from './events/event-list/event-list.component';
+import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
+
+const routes: Routes = [
+  { path: 'home', component: LandingPageComponent },
+  { path: 'events', component: EventListComponent },
+
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: '**', component: PageNotFoundComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
+  declarations: [PageNotFoundComponent]
+})
+export class AppRoutingModule {}
