20d339ba0c18f3261fda492eda95cdc813fb2905
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-17 17:55:22 +0100

    Commit 34

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,12 +1,29 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
 
-
+// Angular material
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatButtonModule } from '@angular/material/button';
+import { MatListModule } from '@angular/material/list';
+import { MatCardModule } from '@angular/material/card';
+import 'hammerjs';
 
 @NgModule({
   declarations: [],
   imports: [
-    CommonModule
-  ]
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule
+  ],
+  exports: [
+    BrowserAnimationsModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatListModule,
+    MatCardModule
+  ],
+  entryComponents: []
 })
-export class SharedModule { }
+export class SharedModule {}
