a4d07d5693440e7633d2b4199c33d80485593697
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-18 16:33:11 +0100

    Commit 49

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,11 +2,12 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
 // Modules
+import { CoreModule } from './core/core.module';
 import { SharedModule } from './shared/shared.module';
+import { AppRoutingModule } from './app-routing.module';
 import { EventsModule } from './events/events.module';
 import { LoginModule } from './login/login.module';
 import { ProfileModule } from './profile/profile.module';
-import { AppRoutingModule } from './app-routing.module';
 
 // Components
 import { AppComponent } from './app.component';
@@ -20,6 +21,7 @@ import { ToolbarComponent } from './toolbar/toolbar.component';
     ToolbarComponent
   ],
   imports: [
+    CoreModule,
     BrowserModule,
     AppRoutingModule,
     SharedModule,
