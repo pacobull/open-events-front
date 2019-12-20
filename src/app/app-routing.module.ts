ec454f9f91933e115ed1aeacbd782df3c7116098
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-20 20:56:20 +0100

    Commit 56

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,12 +6,14 @@ import { EventListComponent } from './events/event-list/event-list.component';
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component';
 import { ProfileComponent } from './profile/profile/profile.component';
 import { LoginComponent } from './login/login/login.component';
+import { EventDetailsComponent } from './events/event-details/event-details.component';
 
 const routes: Routes = [
   { path: 'home', component: LandingPageComponent },
   { path: 'events', component: EventListComponent },
   { path: 'login', component: LoginComponent },
   { path: 'profile', component: ProfileComponent },
+  { path: 'eventDetails/:id', component: EventDetailsComponent },
 
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: '**', component: PageNotFoundComponent }
