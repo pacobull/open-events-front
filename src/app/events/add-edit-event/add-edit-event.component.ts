a7e85bc0577cf7ae5b62be8b319d01688d36411b
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-21 08:57:35 +0100

    Commit 61

---
+++ src/app/events/add-edit-event/add-edit-event.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'oevents-add-edit-event',
+  templateUrl: './add-edit-event.component.html',
+  styleUrls: ['./add-edit-event.component.scss']
+})
+export class AddEditEventComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/events/events.module.ts
+++ src/app/events/events.module.ts
@@ -7,10 +7,11 @@ import { SharedModule } from '../shared/shared.module';
 // Components
 import { EventListComponent } from './event-list/event-list.component';
 import { EventDetailsComponent } from './event-details/event-details.component';
+import { AddEditEventComponent } from './add-edit-event/add-edit-event.component';
 
 @NgModule({
   imports: [CommonModule, SharedModule],
-  declarations: [EventListComponent, EventDetailsComponent],
+  declarations: [EventListComponent, EventDetailsComponent, AddEditEventComponent],
   exports: [EventListComponent, EventDetailsComponent]
 })
 export class EventsModule {}
