84942dffa16f79a5d0294d28673f141fb73f1961
francisco.toro.mateo <francisco.toro.mateo@accenture.com>
2019-12-26 11:04:46 +0100

    Commit 89

--- src/app/events/add-edit-event/add-edit-event.component.ts
+++ src/app/events/add-edit-event/add-edit-event.component.ts
@@ -4,6 +4,7 @@ import { Event } from '../../models/event';
 import { EventService } from '../../core/event.service';
 import { Router } from '@angular/router';
 import { ActivatedRoute } from '@angular/router';
+import { User } from '../../models/user';
 
 @Component({
   selector: 'oevents-add-edit-event',
@@ -58,7 +59,11 @@ export class AddEditEventComponent implements OnInit {
   }
 
   onSubmit() {
+    const user: User = JSON.parse(localStorage.getItem('user'));
+
     this.event = this.addEditForm.value;
+    this.event.addedBy = user.email;
+
     if (this.event.id) {
       this.eventService.updateEvent(this.event).subscribe((event: Event) => {
         console.log(event);
